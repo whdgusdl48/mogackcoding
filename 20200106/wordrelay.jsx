35d5b90394db2413fdbd0f5bb4e19fc44cf82323
whdgusdl48 <52066712+whdgusdl48@users.noreply.github.com>
2020-01-06 16:07:50 +0900

    2020/01/06모각코 코드

---
+++ 20200106/wordrelay.jsx
@@ -0,0 +1,41 @@
+const React = require('react');
+const { Component } = React;
+
+const WordRelay = () => {
+    const [word, setWord] = React.useState('상아');
+    const [value, setValue] = React.useState('');
+    const [result, setResult] = React.useState('');
+    const inputRef = React.useRef(null);
+
+    const onChangeInput = (e) => {
+        setValue(e.target.value);
+    }
+
+    const onSubmitForm = (e) => {
+        e.preventDefault();
+        if (word[word.length - 1] === value[0]) {
+            setResult('딩동댕!!!!!');
+            setWord(value);
+            setValue('');
+        } else {
+            setResult('땡!');
+            setValue('');
+        }
+
+        inputRef.current.focus();
+    }
+
+
+    return (
+        <>
+            <div>{word}</div>
+            <form onSubmit={onSubmitForm}>
+                <label id="label" htmlFor="wordInput">글자를 입력하세요</label><br></br>
+                <input id="wordInput" htmlFor="wordInput" onChange={onChangeInput} ref={inputRef} value={value}/>
+                <button>입력</button>
+            </form>
+            <div>{result}</div>
+        </>
+    );
+};
+module.exports = WordRelay;
